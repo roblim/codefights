34d7d7c3b5b42812f9926c2c72077252a7399a3e
Rob Lim <robert.m.lim@gmail.com>
2017-12-04 20:05:06 -0800

    Start rotateImage

--- interview_practice/arrays.rb
+++ interview_practice/arrays.rb
@@ -17,3 +17,31 @@ def firstNotRepeatingCharacter(s)
     result = result.select { |key, val| val == 1 }
     result.keys.length > 0 ? result.to_a[0][0] : "_"
 end
+
+######
+def rotateImage(a)
+   size = a.length
+   hold = nil
+   a.each_with_index do |row, row_idx|
+      row.each_with_index do |el, col_idx|
+         new_loc = new_coord(size, row_idx, col_idx)
+         hold =
+end
+
+def new_coord(n, r, c)
+   [c, n - r - 1] 
+end
+      
+def cycle(arr, n, row, col)
+   count = 0
+   new_pos = new_coord(n, row, col)
+   hold = arr[row][col]
+   while count < 4
+      hold, arr[new_pos[0]][new_pos[1]] = arr[new_pos[0]][new_pos[1]], hold
+      new_pos = new_coord(n, new_pos[0], new_pos[1])
+      count += 1
+   end
+   arr
+end
+
+######
